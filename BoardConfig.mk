cd1a50cd0e93b7b3a9ef91c4374c8c62d310058e
msfkonsole <msfkonsole@gmail.com>
2016-04-26 12:58:18 +0700

    remove CPU UC & OC
    also tweak Boardconfig.mk
    
    Signed-off-by: msfkonsole <msfkonsole@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,8 +24,8 @@ TARGET_CPU_ABI 				:= armeabi-v7a
 TARGET_CPU_ABI2 			:= armeabi
 TARGET_CPU_SMP 				:= true
 TARGET_CPU_VARIANT 			:= cortex-a7
-TARGET_GLOBAL_CFLAGS 		+= -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS 		+= -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CFLAGS 		+= -mfpu=neon-vfpv4 -mfloat-abi=hard
+TARGET_GLOBAL_CPPFLAGS 		+= -mfpu=neon-vfpv4 -mfloat-abi=hard
 ART_USE_OPTIMIZING_COMPILER := true
 
 # Platform
